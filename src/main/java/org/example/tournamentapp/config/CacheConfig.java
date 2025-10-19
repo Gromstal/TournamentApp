0ab49f14297f36c20b70ca536b96e8bc72579596
Danil Morozov <gromstal@gmail.com>
2025-10-19 19:49:14 +0300

    add VpOpp stat + fix ui

--- src/main/java/org/example/tournamentapp/config/CacheConfig.java
+++ src/main/java/org/example/tournamentapp/config/CacheConfig.java
@@ -1,9 +1,9 @@
 package org.example.tournamentapp.config;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
+
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCache;
+
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,15 +15,14 @@ import java.util.concurrent.TimeUnit;
 @EnableCaching
 public class CacheConfig {
 
-    @Bean
-    public CacheManager cacheManager() {
-        CaffeineCache tasksCache = new CaffeineCache("cache",
-                Caffeine.newBuilder()
-                        .expireAfterWrite(10, TimeUnit.MINUTES)
-                        .maximumSize(1000)
-                        .build());
-        SimpleCacheManager cacheManager = new SimpleCacheManager();
-        cacheManager.setCaches(List.of(tasksCache));
-        return cacheManager;
+//    @Bean
+//    public CacheManager cacheManager() {
+//        CaffeineCache tasksCache = new CaffeineCache("cache",
+//                Caffeine.newBuilder()
+//                        .expireAfterWrite(10, TimeUnit.MINUTES)
+//                        .maximumSize(1000)
+//                        .build());
+//        SimpleCacheManager cacheManager = new SimpleCacheManager();
+//        cacheManager.setCaches(List.of(tasksCache));
+//        return cacheManager;
     }
-}
